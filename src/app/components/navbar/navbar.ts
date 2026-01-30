4203b53e209510427560e472fdac639500cdbbbe
Yago Souza Oliveira <yagoliveira92@gmail.com>
2026-01-30 09:20:18 -0300

    Primeiro Commit

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  standalone: true,
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+
+}
