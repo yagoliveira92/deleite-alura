b730f4d8aa9299a1218cee1eef6285485791a61a
Yago Souza Oliveira <yagoliveira92@gmail.com>
2026-01-30 11:52:11 -0300

    Video 3.1 e 3.2 - Add milkshake data and implement product grid in home page

--- src/app/components/home-page/home-page.ts
+++ src/app/components/home-page/home-page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ProductCard } from '../product-card/product-card';
+import { Product } from '../../interfaces/product';
+import { milkshakes } from '../../data/products';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
 export class HomePage {
-
+  milkshakes: Product[] = milkshakes;
 }
---
+++ src/app/data/products.ts
@@ -0,0 +1,34 @@
+import { Product } from "../interfaces/product"
+
+export const milkshakes: Product[] = [
+    {
+    id: 1,
+    title: 'Milkshake de Amora',
+    price: 18.0,
+    image: 'assets/imagens/milkshakes/milkshake-amora.png',
+  },
+  {
+    id: 2,
+    title: 'Milkshake de Maracuja',
+    price: 18.0,
+    image: 'assets/imagens/milkshakes/milkshake-maracuja.png',
+  },
+  {
+    id: 3,
+    title: 'Milkshake de Morango',
+    price: 18.0,
+    image: 'assets/imagens/milkshakes/milkshake-morango.png',
+  },
+  {
+    id: 4,
+    title: 'Milkshake de Verde',
+    price: 18.0,
+    image: 'assets/imagens/milkshakes/milkshake-verde.png',
+  },
+    {
+    id: 5,
+    title: 'Milkshake de Pistache',
+    price: 18.0,
+    image: 'assets/imagens/milkshakes/milkshake-verde.png',
+  }
+]
