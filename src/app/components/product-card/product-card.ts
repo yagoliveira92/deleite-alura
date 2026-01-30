33d5a80f4606a90a588d821ffe8c4040165afe54
Yago Souza Oliveira <yagoliveira92@gmail.com>
2026-01-30 10:42:10 -0300

    Video 2.4 - Add product card component with styles and tests

---
+++ src/app/components/product-card/product-card.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { Product } from '../../interfaces/product';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css',
+})
+export class ProductCard {
+  product = input.required<Product>();
+}
